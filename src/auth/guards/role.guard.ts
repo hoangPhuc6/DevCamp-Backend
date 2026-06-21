6f1524b0b5e541d4985d89ddb82c3cc185191666
{LUANNGUYEN} <{nguyenthanhluan13022006@gmail.com}>
2026-06-21 09:27:04 +0700

    Push again

--- src/auth/guards/role.guard.ts
+++ src/auth/guards/role.guard.ts
@@ -6,19 +6,19 @@ export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
 @Injectable()
 export class RoleGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
-
   canActivate(context: ExecutionContext): boolean {
-    const requiredRole = this.reflector.getAllAndOverride<string[]>('roles', [
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
       context.getHandler(),
       context.getClass(),
     ]);
-
-    if (!requiredRole) return true;
+    // Check if this route requires any specific roles.
+    if (!requiredRoles) return true;
     const request = context
       .switchToHttp()
       .getRequest<{ user?: { role?: string } }>();
+    // Check the user for any roles => block
     const user = request.user;
     if (!user?.role) return false;
-    return requiredRole.includes(user.role);
+    return requiredRoles.includes(user.role);
   }
 }
