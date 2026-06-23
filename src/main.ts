80e7ec00fe68fba21ed09d5f730c5a4312e29f91
{LUANNGUYEN} <{nguyenthanhluan13022006@gmail.com}>
2026-06-23 22:51:11 +0700

    import ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import morgan from 'morgan'; // HTTP status log
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Tự động convert plain object từ request thành DTO class
+      whitelist: true, // Tự động lọc bỏ các field thừa không được định nghĩa trong DTO
+    }),
+  );
   app.use(morgan('dev')); // define use of morgan middleware
   await app.listen(process.env.PORT ?? 3000);
 }
