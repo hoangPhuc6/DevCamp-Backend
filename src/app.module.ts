a369c4c706b67e8430e7afb41fba8b3ed6a9eab2
{LUANNGUYEN} <{nguyenthanhluan13022006@gmail.com}>
2026-06-23 22:52:05 +0700

    import ChallengesModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ChallengesModule } from './challenges/challenges.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     UserModule,
     AuthModule,
+    ChallengesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
